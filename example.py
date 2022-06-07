84f1beccc81d2a8238c21c2e04b1d9880e6f5562
Samuel Leblanc <samueleblanc17@gmail.com>
2022-06-07 17:12:35 -0400

    added example

--- example.py
+++ example.py
@@ -1,9 +1,14 @@
+"""
+    Simple example on how to use SolvingRT !
+"""
+
 import solvingrt.solve as srt
 
 PATH_TO_VIDS = "/home/user/videos/training_videos/"
 
-ath = srt.Athlete(185, 90, 0.3, 15, "left")
-ex = srt.Exercise("Preacher curl", "biceps", PATH_TO_VIDS + "biceps/preacher_curl.mp4", ath, ["angles", "work"])
+ath = srt.Athlete(height_meter=1.85, body_weight_kg=90, moving_limb_meter=0.3, weight_used_kg=15, side_seen="left")
+ex = srt.Exercise(exercise_name="Preacher curl", muscle="biceps", path_to_video=PATH_TO_VIDS + "biceps/preacher_curl.mp4", 
+                  athlete=ath, measures=["angles", "work"])
 
 ex.video_resize(1280, 720)
 ex.angle_with_gravity()
